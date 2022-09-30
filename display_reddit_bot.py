eca6bbf9d73c683e453d00d10d48cfc1f1c3b827
Nijaz Kovacevic <nijaz.kovacevic@snhu.edu>
2022-09-30 12:34:46 -0700

    Update 1.1.1

---
+++ display_reddit_bot.py
@@ -0,0 +1,46 @@
+import sqlite3
+import os
+
+
+# Connect to the database
+def sql_connection():
+    path = os.path.abspath('RedditDatabase.db')
+    contact = sqlite3.connect(path)
+    return contact
+
+
+# Function designed to retrive all relevant information from the reddit and store it in the database
+def sql_fetcher(contact):
+
+    # SQL search query for rows and columns of the subreddit
+    subreddit = input("\nEnter subreddit to search: r/")
+    count = 0
+    cur = contact.cursor()
+    cur.execute('SELECT * FROM posts') 
+    rows = cur.fetchall()
+
+    for r in rows:
+        if subreddit in r:
+            count += 1
+            print(f'\nTAG: {r[1]}\nPOST TITLE: {r[2]}\nAUTHOR: {r[3]}\n'
+                  f'TIME STAMP: {r[4]}\nUPVOTES: {r[5]}\nCOMMENTS: {r[6]}'
+                  f'\nURL: {r[7]}\n')
+
+    if count:
+        print(f'{count} posts fetched from database\n')
+    else:
+        print('\nNo posts stored for this subreddit\n')
+
+
+
+contact = sql_connection()
+
+while 1:
+    sql_fetcher(contact)
+
+    ans = input('\nPress (y) to continue or any other key to exit: ').lower()
+    if ans == 'y':
+        continue
+    else:
+        print('\nExiting..\n')
+        break
---
+++ fetch_reddit_bot.py
@@ -0,0 +1,54 @@
+import requests
+import csv
+import time
+import sqlite3
+from bs4 import BeautifulSoup
+
+# Connect to the database.
+def sql_connection():
+    contact = sqlite3.connect('SubredditDatabase.db')
+    return contact
+
+# Creating the database tables.
+def sql_table(contact):
+    cur = contact.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS posts(SUBREDDIT text, TAG text, "
+                " TITLE text, AUTHOR text, TIMESTAMP text, UPVOTES int, " 
+                " COMMENTS text, URL text)")
+    contact.commit()
+
+# Insert recorded data into the sql query.
+def sql_insert_table(contact, entities):
+    cur = contact.cursor()
+    cur.execute('INSERT INTO posts(SUBREDDIT, TAG, TITLE, AUTHOR, '
+                'TIMESTAMP, UPVOTES, COMMENTS, URL) '
+                'VALUES(?, ?, ?, ?, ?, ?, ?, ?)', entities)
+    contact.commit()
+
+
+# Scrape the subreddit to find the correct information.
+def scraper():
+    contact = sql_connection()
+    sql_table(contact)
+
+    while 1:
+        subreddit = input('\n\nEnter the name of the subreddit: r/').lower()
+        max_count = int(input('Enter the maximum number of entries to collect: '))
+        select = int(input('Select tags to add for the search: \n1. hot\n2. new'
+                            '\n3. rising\n4. top\n5. Make your choice: '))
+
+        if select == 1:
+            tag = 'hot'
+            tag_url = '/'
+        elif select == 2:
+            tag = 'new'
+            tag_url = '/new/'
+        elif select == 3:
+            tag = 'rising'
+            tag_url = '/rising/'
+        elif select == 4:
+            tag = 'top'
+            tag_url = '/controversial/'
+        elif select == 5:
+            tag = 'top'
+            tag_url = '/top/'
