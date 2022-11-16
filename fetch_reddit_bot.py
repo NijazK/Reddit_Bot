e992d0e2e78dc7bd2e2fb7584c36f36dde293104
Nijaz Kovacevic <nijaz.kovacevic94@gmail.com>
2022-11-16 10:35:20 -0800

    Update fetch.py
    Finished the fetch.py program using design patterns in Python.

--- fetch_reddit_bot.py
+++ fetch_reddit_bot.py
@@ -53,15 +53,91 @@ def scraper():
             tag = 'top'
             tag_url = '/top/'
             
-        url =
+        url = 'https://reddit.com/r/' + subreddit
         
-        req
+        headers = {'User-Agent': 'Mozilla/5.0}
+        
+        required = requests.get(url, headers = headers)
         
         if req.status_code == 200:
-            soup
+            soup = BeautifulSoup(required.text, 'html.parser')
             print
             
             attributes = 
             counter = 1
             full = 0
             reddit_info = []
+            while 1:
+                   for posts in soup.find_all('div', attributes = attributes):
+                    try:
+                        # To obtain the post title
+                        title = post.find('a', class_='title').text
+                   
+                        # To get the username of the post author
+                        author = post.find('a', class_='author').text
+                        
+                        # To obtain the time of the post
+                        time_stamp = post.Time.attributes['title']
+                   
+                        # To obtain the number of comments on the post
+                        comments = post.find('a', class_='comments').text.split()[0]
+                        if comments == 'comments':
+                             comments = 0
+                        
+                        # To get upvotes on the post
+                        upvotes = post.find('div', class_='score likes').text
+                        if upvotes == '.':
+                            upvotes = "None"
+                        
+                        link = post.find('a', class_='title')['href']
+                        link = 'www.reddit.com' + link
+                   
+                        # Entering all the collected information into our database
+                        entities = (subreddit, tag, title, author, time_stamp, upvotes,
+                                    comments, link)
+                        sql_insert_table(contact, entities)
+                   
+                        if counter == max_count:
+                            full = 1
+                            break
+                        
+                        counter += 1
+                    except AttributeError:
+                        continue
+                   
+                   if full:
+                        break
+                  
+                   try:
+                        next_button = soup.find('span', class_='next_button')
+                        next_page = next_button.find('a').attributes['href]
+                        
+                        time.sleep(2)
+                                                     
+                        required = requests.get(next_page_link, headers=headers)
+                        soup = BeautifulSoup(required.text, 'html.parser')
+                   except:
+                       break
+                                                                     
+              print('Finished\n')
+              answer = input('Press (y) to continue or (n) to exit: ').lower()
+              if answer == 'y' || answer == 'Y':
+                   continue
+              else if answer == 'n' || answer == 'N'
+                      print('Exiting')
+                      break
+            else:
+                print('Error occured')
+                                                                     
+if __name__ == '__main__':
+    scarper()                                                   
+                                                                     
+                                                                     
+                                                                     
+                                                                     
+                                                                     
+                                                                     
+                                                                     
+                                                                     
+                                                               
+                                                                     
